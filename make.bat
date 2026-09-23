48e660eea8cf45ef056918aa9d94a9e1a5f6c145
undefined06855 <currentcashew57@gmail.com>
2026-09-23 21:21:08 +0100

    make build script auto copy

--- make.bat
+++ make.bat
@@ -1,2 +1,6 @@
 @echo off
 ..\..\bin\make.exe %*
+
+REM change this if different for your system, might consider a better build system in the future but there is too sparse
+REM documentation on this... does everyone have their prizm make two folders up? it's pretty annoying
+copy minesweeper.g3a D:\minesweeper.g3a
